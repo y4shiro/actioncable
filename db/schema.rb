49d976ebcdc39660f1cbea2b6c370c85a8e495c3
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-05-31 18:04:39 +0900

    generate message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170531090413) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
