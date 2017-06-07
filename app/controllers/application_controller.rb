ce5ea28fb13cb3c033dca3815ac7435626c4cb87
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-06-07 18:43:39 +0900

    Add online user count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  $onlineusers = User.where(online: true)
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
